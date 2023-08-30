2b9a6ffe57725d03830e1fe403554765f4113ea8
Sergio Guerrero <serfresh@hotmail.com>
2023-08-29 19:33:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const axios = require("axios");
-
+const app = express();
 const port = 443; // Puerto en el que escuchará el servidor
 
 const nodes = [
